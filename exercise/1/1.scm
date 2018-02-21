5cc767cf328755a0ded8112a134e33fc28bd00d4
Tatsuya Katsushima <t.katsushima@outlook.jp>
2018-02-21 23:21:22 +0900

    update

--- exercise/1/1.scm
+++ exercise/1/1.scm
@@ -43,15 +43,37 @@
 
 
 
-
-;; 1.6
-
-
-
+;; Square roots by Newton's method
+(define (square x) (* x x))
 
 (define (sqrt-iter guess x)
   (if (good-enough? guess x)
       guess
       (sqrt-iter (improve guess x)
                  x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+
+
+;; 1.6
+
+;; 1.7
+(define (good-enough? guess x)
+  (< (abs (- guess (/ x guess))) 0.000001))
+
+;; 1.8
+(define (improve guess x)
+  (/ (+ (/ x (square y)) (* 2 y))
+     3))
 
