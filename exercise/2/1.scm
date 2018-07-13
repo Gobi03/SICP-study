1b033b83e26bc486ebb8a6daefb8f227358db7c1
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2018-07-13 13:03:12 +0900

    update

--- exercise/2/1.scm
+++ exercise/2/1.scm
@@ -36,3 +36,63 @@
 
 
 (make-segment 1 4)
+
+
+;; 2.3
+(define (make-rectangle leftup rightdown)
+  (cons leftup rightdown))
+
+(define (make-perimeter rectangle)
+  (let* ((left-up (car rectangle))
+         (right-down (cdr rectangle))
+         (x1 (x-point left-up))
+         (y1 (y-point left-up))
+         (x2 (x-point right-down))
+         (y2 (y-point right-down)))
+    (+ (* (abs (- x1 x2)) 2)
+       (* (abs (- y1 y2)) 2))))
+
+(define (make-area rectangle)
+  (let* ((left-up (car rectangle))
+         (right-down (cdr rectangle))
+         (x1 (x-point left-up))
+         (y1 (y-point left-up))
+         (x2 (x-point right-down))
+         (y2 (y-point right-down)))
+    (* (abs (- x1 x2))
+       (abs (- y1 y2)))))
+
+(make-perimeter (make-rectangle (make-point -1 -1)
+                                (make-point 2 2)))
+(make-area (make-rectangle (make-point -1 -1)
+                           (make-point 2 2)))
+
+(define (rect)
+  (make-rectangle (make-point -1 -1)
+                  (make-point 2 2)))
+
+;; 2.5
+(define (cons x y)
+  (* (expt 2 x)
+     (expt 3 y)))
+
+(define (div x d)
+  (if (= (modulo x d) 0)
+      (div (/ x d) d)
+      x))
+
+(define (car z)
+  (log (div z 3) 2))
+
+(define (cdr z)
+  (log (div z 2) 3))
+
+
+(car 24)
+(cdr 24)
+
+(define one
+  (lambda (f) (lambda (x) (f x))))
+
+(define two
+  (lambda (f) (lambda (x) (f (f x)))))
