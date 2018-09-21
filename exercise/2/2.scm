479a5c1f1e6ea5e52e398b2facb636677a57589e
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2018-09-21 12:20:39 +0900

    18.09.20回

--- exercise/2/2.scm
+++ exercise/2/2.scm
@@ -230,3 +230,47 @@
 
 (define (reverse sequence)
   (foldl (lambda (x y) (cons x y)) null sequence))
+
+
+
+
+
+
+(define wave
+  einstein)
+(define wave2
+  (beside wave (flip-vert wave)))
+(define wave4
+  (below wave2 wave2))
+
+(define (right-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (right-split painter (- n 1))))
+        (beside painter (below smaller smaller)))))
+
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left (beside up up))
+              (bottom-right (below right right))
+              (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+
+(define (square-limit painter n)
+  (let ((quarter (corner-split painter n)))
+    (let ((half (beside (flip-horiz quarter) quarter)))
+      (below (flip-vert half) half))))
+
+
+
+(define (square-of-four tl tr bl br)
+  (lambda (painter)
+    (let ((top (beside (tl painter) (tr painter)))
+          (bottom (beside (bl painter) (br painter))))
+      (below bottom top))))
+
+
