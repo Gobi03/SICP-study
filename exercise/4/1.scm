95ec258357911e0548cf5f6de76b9c0527a2b9f3
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2019-04-18 19:23:52 +0900

    19.04.18回

--- exercise/4/1.scm
+++ exercise/4/1.scm
@@ -40,3 +40,53 @@
       (cons (eval (first-operand exps) env)
             (list-of-values (rest-operands exps) env))))
 
+
+;; 1.3
+
+
+
+;; 1.4
+(define (setup-environment)
+  (let ((initial-env
+         (extend-environment (primitive-procedure-names)
+                             (primitive-procedure-objects)
+                             the-empty-environment)))
+    (define-variable! 'true true initial-env)
+    (define-variable! 'false false initial-env)
+    initial-env))
+
+
+(define the-global-environment (setup-environment))
+
+
+(define input-prompt ";;; M-Eval input:")
+(define output-prompt ";;; M-Eval value:")
+
+
+(define (driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((input (read)))
+    (let ((output (eval input the-global-environment)))
+      (announce-output output-prompt)
+      (user-print output)))
+  (driver-loop))
+
+
+(define (prompt-for-input string)
+  (newline) (newline) (display string) (newline))
+
+
+(define (announce-output string)
+  (newline) (display string) (newline))
+
+(define (user-print object)
+  (if (compound-procedure? object)
+      (display (list 'compound-procedure
+                     (procedure-parameters object)
+                     (procedure-body object)
+                     '))
+      (display object)))
+
+(define the-global-environment (setup-environment))
+
+(driver-loop)
